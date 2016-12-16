67159b57e9d7cdfca299c4b6a0e5717abcd9bc48
hanangjie <hanangjie@gmail.com>
2016-12-16 14:46:26 +0800

    degub

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.get('/getLike', function(req, res, next) {
 });
 
 router.get('/saveHtml', function(req, res, next) {
-	 fs.writeFile('./file/'+req.query.name+'.html', `${req.query.html}`, (err) => {
+	 fs.writeFile('./file/'+req.query.name+'.html', `${req.query.html.replace(/\*/g,"https://vtt.tumble.com")}`, (err) => {
         if (err) throw err;
         console.log('It\'s saved!');
     });
