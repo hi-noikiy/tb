dbfa51da13bcd53e882dfa542347d0c78c1e3b2d
hanangjie <hanangjie@gmail.com>
2016-12-16 10:10:37 +0800

    de

--- routes/index.js
+++ routes/index.js
@@ -11,23 +11,23 @@ router.get('/', function(req, res, next) {
 });
 router.get('/getcont', function(req, res, next) {
 	var uri={
-		video:'https://api.tumblr.com/v2/blog/'+req.param('name')+
-		'.tumblr.com/posts/video?api_key=MTSQG9xC2tAAgK6y8AHKvVFpI3QGIKPRDx9O0Iybpfls3DTfYc&limit='+(req.param('limit')||20)+
-		"&offset="+req.param('offset'||0),
-		photo:'https://api.tumblr.com/v2/blog/'+req.param('name')+
-		'.tumblr.com/posts/photo?api_key=MTSQG9xC2tAAgK6y8AHKvVFpI3QGIKPRDx9O0Iybpfls3DTfYc&limit='+(req.param('limit')||20)+
-		"&offset="+req.param('offset'||0)
+		video:'https://api.tumblr.com/v2/blog/'+req.query.name+
+		'.tumblr.com/posts/video?api_key=MTSQG9xC2tAAgK6y8AHKvVFpI3QGIKPRDx9O0Iybpfls3DTfYc&limit='+(req.query.limit||20)+
+		"&offset="+(req.query.offset||0),
+		photo:'https://api.tumblr.com/v2/blog/'+req.query.name+
+		'.tumblr.com/posts/photo?api_key=MTSQG9xC2tAAgK6y8AHKvVFpI3QGIKPRDx9O0Iybpfls3DTfYc&limit='+(req.query.limit||20)+
+		"&offset="+(req.query.offset||0),
 	}
-	 request({uri:uri[req.param('type')],encoding: null}, function (error, response, body) {
+	 request({uri:uri[req.query.type],encoding: null}, function (error, response, body) {
 	    var html1=iconv.decode(body, 'utf8');
   		res.send(html1);
 	});
 });
 
 router.get('/getLike', function(req, res, next) {
-	 request({uri:"https://api.tumblr.com/v2/blog/"+req.param('name')+
-		".tumblr.com/likes?api_key=MTSQG9xC2tAAgK6y8AHKvVFpI3QGIKPRDx9O0Iybpfls3DTfYc&limit="+(req.param('limit')||20)+
-		"&offset="+req.param('offset'||0),encoding: null}, function (error, response, body) {
+	 request({uri:"https://api.tumblr.com/v2/blog/"+req.query.name+
+		".tumblr.com/likes?api_key=MTSQG9xC2tAAgK6y8AHKvVFpI3QGIKPRDx9O0Iybpfls3DTfYc&limit="+(req.query.limit||20)+
+		"&offset="+(req.query.offset||0)}, function (error, response, body) {
 	    var html1=iconv.decode(body, 'utf8');
   		res.send(html1);
 	});
