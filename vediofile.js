3198957058d2ed8a41cbbf7ec89ef9c7be8bdd73
hanangjie <hanangjie@gmail.com>
2017-04-12 22:31:51 +0800

    d

--- vediofile.js
+++ vediofile.js
@@ -1,23 +1,14 @@
 var fs=require("fs")
 
 
-fs.readdir("file",(err,files)=>{
-   var html="",q=0;
-  files.forEach((e,i)=>{
-  		fs.stat("./file/"+e, (err, stats) => {
-		  if(stats.size<10000&&!(e.indexOf("$")!=-1)){
-			html+=fs.readFileSync("./file/"+e,'utf8');
-			fs.renameSync("./file/"+e, "./file/$"+e)
-		  }
-		  console.log(q)
-		  q++;
-		  if(q==files.length){
-		  	fs.writeFile('total.html', html, (err) => {
-			 
-			  console.log('It\'s saved!');
-			});
-		  }
+/*fs.readdir("tb",(err,files)=>{
+   	var html="",q=0;
+	files.forEach((e,i)=>{
+		fs.stat("./file/"+e, (err, stats) => {
+		  
 		});
-  })
-  
-})
+	})
+})*/
+fs.stat("./tb/tumblr_nwf1ukRLXk1uib5bj.mp4", (err, stats) => {
+		  console.log(stats)
+});
