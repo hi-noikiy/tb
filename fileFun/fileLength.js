b93864b55be629330240714e4ccd6c61dd7280ef
hanangjie <hanangjie@gmail.com>
2017-04-14 14:52:42 +0800

    debug

--- fileFun/fileLength.js
+++ fileFun/fileLength.js
@@ -1,20 +1,42 @@
 var fs=require("fs")
 
 fs.readdir("file",(err,files)=>{
-   var html="",arr=[];
+   var html="",arr=[],newArr=[];
    files.forEach((e,i)=>{
    		 html+=fs.readFileSync("./file/"+e,'utf8');
    })
    arr=html.split("</li><li>")
 
+   //去除非连接
+   arr.forEach((e,i)=>{
+         if(e.indexOf("tumblr")!=-1){
+            newArr.push(e)
+         }
+   })
+   arr=[];
+   //去除重复连接
+   newArr.forEach((e,i)=>{
+      var canInt=true;
+         arr.forEach((q,y)=>{
+            if(q==e){
+               canInt=false;
+            }
+         })
+         if(canInt){
+            arr.push(e);
+         }
+   })
+
+   console.log(newArr.length,arr.length)
+
    arr.forEach((e,i)=>{
    		if(i%800==0){
-   			var newArr=arr.slice(i,(i+800))
-   			console.log(newArr.length)
+   			let newArr=arr.slice(i,(i+800))
+   			/*console.log(newArr.length)
    			let html=+"<li>"+newArr.length+"</li>"+newArr.join("</li><li>")
    			 fs.writeFile('./1000file/total'+i+".html", html, (err) => {
 				  console.log('It\'s saved!');
-			});
+			   });*/
    		}
    })
   
